b919b94beb7517c38261c24b5610471d00289af9
hahagarden <tingyuan413@gmail.com>
2022-06-07 00:34:31 +0900

    save username(localStorage)
    * localStorage(key, value)
    * make paintGreetings function

--- chrome-app/app.js
+++ chrome-app/app.js
@@ -3,14 +3,27 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden"; //one of the rules, upperCase is used when it contains only strings and not that important data
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   // the first argument, event, is JS automatically offers the user the information about the event when it occurs
   event.preventDefault(); // event has Default actions and we often prevent them from occurring for next works
-  const username = loginInput.value;
+  const typedusername = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, typedusername); // saving data on browser API, localStorage
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  greeting.innerText = `Hello ${username}!`; // combine strings and variables,  "Hello " + username + "!"
+  paintGreetings(typedusername);
+}
+
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}!`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME); // show the form
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername); // show the greetings
+}
