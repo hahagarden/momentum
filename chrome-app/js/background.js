5d773da6f947a15b98ac4978a010eff699733b95
hahagarden <tingyuan413@gmail.com>
2022-06-12 02:08:36 +0900

    Save in localStorage and Load To-do list
    * JSON.stringify()
    * JSON.parse
    * array.forEach(function)

--- chrome-app/js/background.js
+++ chrome-app/js/background.js
@@ -1,4 +1,15 @@
-const images = ["img/0.jpeg", "img/1.jpeg"];
+const images = [
+  "img/0.jpg",
+  "img/1.jpg",
+  "img/2.jpg",
+  "img/3.jpg",
+  "img/4.jpg",
+  "img/5.jpg",
+  "img/6.jpg",
+  "img/7.jpg",
+  "img/8.jpg",
+  "img/9.jpg",
+];
 
 const chosenImange = images[Math.floor(Math.random() * images.length)];
 
--- chrome-app/js/todo.js
+++ chrome-app/js/todo.js
@@ -1,6 +1,13 @@
 const toDoForm = document.querySelector("#toDo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector("#toDo-list");
+const TODOS_KEY = "todos";
+
+let toDos = []; // let for toDos=savedToDos;
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
 
 function deleteToDo(event) {
   const li = event.target.parentElement; // we can access to the parent element of the button using event.target.parentElement
@@ -22,8 +29,14 @@ function paintToDo(typedInput) {
 function handleToDoSubmit(event) {
   event.preventDefault();
   const typedInput = toDoInput.value;
+  toDos.push(typedInput);
   toDoInput.value = "";
   paintToDo(typedInput);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = JSON.parse(localStorage.getItem(TODOS_KEY));
+toDos = savedToDos;
+savedToDos.forEach(paintToDo); // a function is excuted for each item in array
